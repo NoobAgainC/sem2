33425c70849e4231e76c099ba1804ef40c8227d0
NoobAgainC <100476030+NoobAgainC@users.noreply.github.com>
2022-07-04 00:27:52 +0530

    Removed mistake

--- 04-circle.c
+++ 04-circle.c
@@ -10,7 +10,7 @@ int main()
 
     printf("Enter 1 to get the area of circle or 2 to get the circumference of circle:\n");
     while (choice < 1 || choice > 2) scanf("%d", &choice);
-    printf("Enter the radius of circle(in cm):\n");
+    printf("Enter the radius of circle(in metre):\n");
     scanf("%f", &r);
     choice == 1 ? printf("Area of given circle is %.2f square metres.\n", pi*r*r) : printf("Circumference of given circle is %.2f metres.\n", 2*pi*r);
     getch();
